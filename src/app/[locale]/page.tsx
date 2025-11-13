21baf77fbe0c9720748e784daa43678514e57c06
fshojaei <fatemeeshojaeii@gmail.com>
2025-11-13 20:02:33 +0330

    fix tailwind

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,5 +3,9 @@ import { useTranslations } from "next-intl";
 export default function HomePage() {
   const t = useTranslations("homePage");
 
-  return <h1>{t("title")}</h1>;
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+    </div>
+  );
 }
